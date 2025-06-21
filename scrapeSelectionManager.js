50f9a1c96467284ed4af87043f320bfc85a89988
Shawn Altman <shawn@dreamspectrum.com>
2025-06-20 17:31:44 -0700

    fix selection overlay reinjection

--- scrapeSelectionManager.js
+++ scrapeSelectionManager.js
@@ -174,10 +174,11 @@ function beginManualSelection() {
     selectedElements.push(el);
     highlightElement(el);
     safeSendMessage({ type: 'ELEMENT_ADDED', count: selectedElements.length });
-    alert('Element added for export');
 
     setTimeout(() => {
-      selectorTool.injectOverlay(onSelect);
+      if (manualSelecting) {
+        selectorTool.injectOverlay(onSelect);
+      }
     }, 300);
   }
 
